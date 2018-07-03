5d68488251c1ccb3b7f467038d229e159bd4e91d
tumit <tumit@live.com>
2018-07-04 00:51:16 +0700

    refactor: move state to app page and bind input to hangman.component instead

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HangmanService, PuzzleState } from './services/hangman.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  public puzz$: Observable<PuzzleState>;
+
+  constructor(private hangman: HangmanService) {
+  }
+
+  ngOnInit() {
+    this.puzz$ = this.hangman.puzzleChanges();
+  }
 }
--- src/app/components/hangman/hangman.component.ts
+++ src/app/components/hangman/hangman.component.ts
@@ -1,21 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, Input, OnInit } from '@angular/core';
 
-import { HangmanService, PuzzleState } from '../../services/hangman.service';
+import { PuzzleState } from '../../services/hangman.service';
 
 @Component({
   selector: 'app-hangman',
   templateUrl: './hangman.component.html',
   styleUrls: ['./hangman.component.css']
 })
-export class HangmanComponent implements OnInit {
-
-  public puzz$: Observable<PuzzleState>;
-
-  constructor(private hangman: HangmanService) {
-  }
-
-  ngOnInit() {
-    this.puzz$ = this.hangman.puzzleChanges();
-  }
+export class HangmanComponent {
+  @Input()
+  public puzz: PuzzleState;
 }
