72bbf933ab0d356a61fae903addda719ceae356a
tumit <tumit@live.com>
2018-07-04 11:37:19 +0700

    fix(button-list.component): fix unit test

--- src/app/components/button-list/button-list.component.spec.ts
+++ src/app/components/button-list/button-list.component.spec.ts
@@ -5,20 +5,18 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { HangmanService } from '../../services/hangman.service';
 
 describe('ButtonListComponent', () => {
-  let hangmanService: HangmanService;
+
   let component: ButtonListComponent;
   let fixture: ComponentFixture<ButtonListComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ButtonListComponent],
-      imports: [HttpClientTestingModule]
+      declarations: [ButtonListComponent]
     }).compileComponents();
-    hangmanService = TestBed.get(HangmanService);
+
     fixture = TestBed.createComponent(ButtonListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-
   }));
 
   it('should create', () => {
@@ -27,10 +25,10 @@ describe('ButtonListComponent', () => {
 
   it('should call to start game when activate start', () => {
     // arrange
-    const startFn = spyOn(hangmanService, 'start');
+    spyOn(component.activatedStart, 'emit');
     // act
     component.start();
     // assert
-    expect(startFn).toHaveBeenCalled();
+    expect(component.activatedStart.emit).toHaveBeenCalled();
   });
 });
